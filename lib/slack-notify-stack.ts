e8b52d26f5e7536f038de492388cd430d0039616
gen <ymdtshm@gmail.com>
2020-10-01 08:08:28 +0900

    refactoring

--- lib/slack-notify-stack.ts
+++ lib/slack-notify-stack.ts
@@ -7,21 +7,23 @@ export class SlackNotifyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const hooks = this.node.tryGetContext("hooks")
+
     const topic = new sns.Topic(this, "slack-notify-topic")
 
-    const slack_notify = new lambda.Function(this, 'slack-notify-lamda', {
-      runtime:  lambda.Runtime.NODEJS_12_X,
-      code:     lambda.Code.asset("src"),
+    const lmd = new lambda.Function(this, 'slack-notify-lamda', {
+      runtime:  lambda.Runtime.RUBY_2_7,
+      code:     lambda.Code.fromAsset("src"),
       handler:  'slack-notify.handler',
       timeout:  cdk.Duration.seconds(60),
       environment: {
-        WEBHOOK_DISASTER: disaster,
-        WEBHOOK_ALART:    alart,
-        WEBHOOK_ERROR:    error,
-        WEBHOOK_WARNIG:   warning
+        WEBHOOK_DISASTER: hooks["disaster"],
+        WEBHOOK_ALART:    hooks["alart"],
+        WEBHOOK_ERROR:    hooks["error"],
+        WEBHOOK_WARNIG:   hooks["warning"],
       }
     });
 
-    slack_notify.addEventSource(new SnsEventSource(topic))
+    lmd.addEventSource(new SnsEventSource(topic))
   }
 }
